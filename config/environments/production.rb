c6046b20f308fdd2984130fa28b3be34197171be
chota5511 <binh_xu_2010@yahoo.com.sg>
2018-09-20 17:26:15 +0700

    0.2.5 - built 3
    --Trying to fix precompile error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "MyCV_#{Rails.env}"
 
-  # Don't care if the mailer can't send.  
+  # Don't care if the mailer can't send.
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
